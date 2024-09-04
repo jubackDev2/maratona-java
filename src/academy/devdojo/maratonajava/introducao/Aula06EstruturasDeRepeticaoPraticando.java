8bb1dc44f057489e3f8bbb50b799c16594611a3e
julianadev <designsilvaport@yahoo.com>
2024-09-04 11:11:30 -0300

    tabuada usando laços de repeticoes

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticaoPraticando.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticaoPraticando.java
@@ -2,12 +2,10 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula06EstruturasDeRepeticaoPraticando {
     public static void main(String[] args) {
-        int tabuada = 2;
+        int i = 2;
 
-        while (tabuada < 11) {
-            System.out.println (" 2 x "+tabuada+" = "+tabuada*2);
-            tabuada += 1;
-
-        }
+       for (i = 0; i <11; i++){
+           System.out.println("2 x "+ i+ " = "+ i*2);
+       }
     }
 }
