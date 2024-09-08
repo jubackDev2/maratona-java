5b5f2ed0aeeffe74f845f8b807c6968776ccada1
julianadev <designsilvaport@yahoo.com>
2024-09-08 13:38:51 -0300

    adicionando alguns comentários

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02.java
@@ -4,10 +4,25 @@ public class Aula06EstruturasDeRepeticao02 {
     public static void main(String[] args) {
         // imprima todos os numeros pares de 0 até 1000000.
 
+        /*
+           podemos fazer de diversas formas
 
-        int i = 0;
+           ---------------------------------------
 
-        for (i=1; i<=1000000; i++) {
+
+           for (int i = 0; i<=1000000; i+=2) {
+                if (i%2 ==0) {
+                System.out.println(i);
+
+                }
+           }
+           ----------------------------------------
+         */
+
+
+
+
+        for (int i=0; i<=1000000; i++) {
             if (i%2 ==0) {
                 System.out.println(i);
             }
