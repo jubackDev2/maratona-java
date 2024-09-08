b823816a8e53d8061f0031cb2e8fd009aed7b806
julianadev <designsilvaport@yahoo.com>
2024-09-08 16:17:04 -0300

    estrutura de repetição, break.

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao03.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao03 {
+    public static void main(String[] args) {
+        // usando o break
+        // imprima os primeiros 25 numeros de um dado valor. por exemplo 50
+
+        int valorMax = 50;
+
+        for (int i = 1; i<= valorMax; i++) {
+            if (i > 25) {
+               break;
+
+            }
+            System.out.println(i);
+        }
+    }
+}
