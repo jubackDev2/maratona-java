74d652b296b1bf88a8f7bd847b193efb241dd952
julianadev <designsilvaport@yahoo.com>
2024-09-09 15:22:06 -0300

    exercicio pra fixar conteudo. laço de repetiçao

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
@@ -1,4 +1,59 @@
 package academy.devdojo.maratonajava.introducao;
 
+import java.io.FileOutputStream;
+
 public class Aula06EstruturasDeRepeticao04 {
+    public static void main(String[] args) {
+        // dado um valor de um carro, descubra, em quantas vezes ele pode ser parcelado
+        // valor de um carro 50 mil
+        // a condição é que o valor da parcela tem que ser >= 1000
+
+
+
+
+
+
+        int valorCarro = 80000;
+
+        for (int parcela = 1; parcela < valorCarro; parcela++) {
+
+            int res = valorCarro/parcela; //
+
+            if (res >= 1000) {
+                System.out.println("parcela::: "+ parcela+" $ "+ res);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
+
+
