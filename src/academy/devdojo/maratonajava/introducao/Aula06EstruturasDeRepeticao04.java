69a264388024f8e5d56722561ab18adc33ba2961
julianadev <designsilvaport@yahoo.com>
2024-09-10 15:14:54 -0300

    fazendo alguns comentários no codigo, pra poder entender cada parte, ja que nao entendi a explicação do professor

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
@@ -9,6 +9,13 @@ public class Aula06EstruturasDeRepeticao04 {
         // a condição é que o valor da parcela tem que ser >= 1000
 
 
+        /*
+         for (int parcela = 1; parcela < valorCarro; parcela++)
+
+         o laço for vai dividir o valor do carro (valorCarro = 80.000) pelo número de parcelas, começando de 1 e indo até o valor do carro (ou seja, 80.000 iterações)
+
+         */
+
 
 
 
@@ -16,12 +23,21 @@ public class Aula06EstruturasDeRepeticao04 {
         int valorCarro = 80000;
 
         for (int parcela = 1; parcela < valorCarro; parcela++) {
-
             int res = valorCarro/parcela; //
 
-            if (res >= 1000) {
+            if (res >= 1000 ) {
+
                 System.out.println("parcela::: "+ parcela+" $ "+ res);
+
+
+
+
+            }else {
+                break;
             }
+
+
+
         }
 
 
