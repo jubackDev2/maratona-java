34932af22e74ce081544249e5382bfbfb04914d8
julianadev <designsilvaport@yahoo.com>
2024-09-10 17:06:38 -0300

    mudei o nome da variavel res, para varParcela. para nao ficar confuso

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
@@ -12,11 +12,11 @@ public class Aula06EstruturasDeRepeticao04 {
         int valorCarro = 80000;
 
         for (int parcela = 1; parcela < valorCarro; parcela++) {  //   vai dividir o valor do carro  pelo número de parcelas, começando de 1 e indo até o valor do carro
-            int res = valorCarro / parcela; //  A variavel "res" que criei calcula o valor da parcela, Então se a parcela for 1, o resultado será 80.000/1...
+            int valorParcela = valorCarro / parcela; //  A variavel "res" que criei calcula o valor da parcela, Então se a parcela for 1, o resultado será 80.000/1...
 
-            if (res >= 1000) {
+            if (valorParcela >= 1000) {  // garante que apenas parcelas onde o valor seja maior ou igual a 1.000 sejam impressas no console.
 
-                System.out.println("parcela::: " + parcela + " $ " + res);
+                System.out.println("parcela::: " + parcela + " $ " + valorParcela);
 
 
             } else {
