243b9af2ea83f3e9c5d8150f23eced07f66ddf64
julianadev <designsilvaport@yahoo.com>
2024-09-11 16:08:01 -0300

    aula 05 finalizada.

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao05.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao05.java
@@ -1,4 +1,16 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula06EstruturasDeRepeticao05 {
+    public static void main(String[] args) {
+        double valorCarro = 30000;
+        for (int parcela = (int)valorCarro; parcela >= 1; parcela--) {
+            double valorParcela = valorCarro / parcela;
+            if (valorParcela < 1000) { // valor menor que mil é ignorado, pois é false e a proxima linha é executada
+                continue;
+            }
+
+            System.out.println("parcela "+ parcela + " $ "+ valorParcela);
+
+        }
+    }
 }
