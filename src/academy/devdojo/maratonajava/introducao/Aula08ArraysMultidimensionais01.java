d0488fc94827d6b63a84e673cd6e1fd57d82f2b6
julianadev <designsilvaport@yahoo.com>
2024-10-11 15:31:39 -0300

    arrays multidimensional

---
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais01 {
+    public static void main(String[] args) {
+        int [] [] dias = new int [3] [3];
+        dias [0] [0] = 31;
+        System.out.println(dias[0][0]);
+
+    }
+}
