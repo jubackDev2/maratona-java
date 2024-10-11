2705d5fed983509e7a966bcec1a2589a31d1061b
julianadev <designsilvaport@yahoo.com>
2024-10-11 00:28:32 -0300

    novas formas de inicializar arrays

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
@@ -3,5 +3,11 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula07Arrays03 {
     public static void main(String[] args) {
         // uma nova forma de inicializar os arrays e imprimir
+
+        int[]numeros2 = {1,2,3,4,5};
+        int[]numeros3 = new int []{1,2,3,4,5};
+        System.out.println(numeros3[1]);
+        System.out.println(numeros2[1]);
+
     }
 }
