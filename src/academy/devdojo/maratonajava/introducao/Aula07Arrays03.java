642e1b3e2744eee78c9b4d5ca99cb4e7fb735eda
julianadev <designsilvaport@yahoo.com>
2024-10-11 00:43:40 -0300

    arrays- foreach

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
@@ -6,8 +6,16 @@ public class Aula07Arrays03 {
 
         int[]numeros2 = {1,2,3,4,5};
         int[]numeros3 = new int []{1,2,3,4,5};
-        System.out.println(numeros3[1]);
-        System.out.println(numeros2[1]);
 
+
+        // for(int i =0; i < numeros3.length; i++){
+        //  System.out.println(numeros3[3]);
+        //}
+        for(int num:numeros3) {
+            System.out.println(num);
+        }
+
+        // nova forma de imprimir que é o : foreach
+        // se voce precisar acessar um indice especifico, va para o for indexado. ->for(int i =0; i < numeros3.length; i++)
     }
 }
