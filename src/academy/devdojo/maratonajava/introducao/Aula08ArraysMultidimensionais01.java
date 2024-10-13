1929a5c4238a040677c92175defae8a57e4c2ed9
julianadev <designsilvaport@yahoo.com>
2024-10-13 00:54:33 -0300

    revisando arrays multidimensional

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -3,8 +3,21 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula08ArraysMultidimensionais01 {
     public static void main(String[] args) {
         int [] [] dias = new int [3] [3];
-        dias [0] [0] = 31;
-        System.out.println(dias[0][0]);
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 30;
+
+        dias[1][0] = 31;
+        dias[1][1]= 28;
+        dias[1][2]= 31;
+
+        for (int i =0; i <  dias.length; i++){
+
+            for (int j  =0; j < dias[0].length; j++){
+                System.out.println(dias[i][j]);
+            }
+        }
+
 
     }
 }
