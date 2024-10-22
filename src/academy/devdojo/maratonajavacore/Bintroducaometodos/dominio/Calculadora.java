ad9f2f0cfcc9b77cf8db5f3793ef076634efa534
julianadev <designsilvaport@yahoo.com>
2024-10-21 21:03:49 -0300

    metodos - parametros  tipo primitivo

--- src/academy/devdojo/maratonajavacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajavacore/Bintroducaometodos/dominio/Calculadora.java
@@ -9,6 +9,26 @@ public class Calculadora {
       vamos começar com o fato de não retornar nada: void e em seguida nos temos o nome do metodo
 
       importante: se voce tem um metodo com mais de 3 argumentos, significa seu metodo está fazendo muita coisa.. não é uma boa pratica
+
+
+      importante: metodo com retorno. Em vez de void, posso usar qualquer tipo de variavel.
+
+      importante: O retorno de um metodo é como voce estivesse inicializando uma variavel, entao precisando colocar dentro de uma variavel, mas nem sempre é necessario.
+
+      ex -> colocando dentro de uma variavel:  double resultado = calculadora.divideDoisNumeros(20, 2); ou
+      ex -> executando direto no console:  System.out.println(calculadora.divideDoisNumeros(20,2));
+
+      -----
+          public double divideDoisNumeros(double num1, double num2){
+         return num1/num2;
+    }
+        int resultado = (int) calculadora.divideDoisNumeros(20, 2); -> fazendo casting
+
+       -----
+
+       importante: podemos usar o return para metodos void, também
+
+       importante: quando voce tem um metodo System.out.println(), você nao pode colcoar dentro dele uma chamada se um metodo que nao retorna um valor
       */
 
     public void somaDoisNumeros(){
@@ -20,11 +40,49 @@ public class Calculadora {
         System.out.println(21-2);
     }
 
-    public void multiplicaDoisNumeros(int num1, int num2){ // parametros nada mais é que variaveis locais
+    public void multiplicaDoisNumeros(int num1, int num2){
         System.out.println(num1*num2);
     }
 
     public double divideDoisNumeros(double num1, double num2){
-         return num1/num2;
+
+        if (num2 == 0){ // se no num2 for zero, ja retorna zero!!
+            return 0; // o return ele funciona mais ou menos como um break, não é que ele vai parar a execução do metodo, mas quando ele chegar nessa linha ele vai voltar pra quem chamou
+        }
+         return num1/num2; // aqui nao vai ser executado!!!!
+
+
     }
+
+    public double divideDoisNumeros02(double num1, double num2){
+        if (num2 !=0 ){
+            return num1/num2;
+        }
+        System.out.println("nao existe divisao por zero");
+        return 0;
+
+    }
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2){
+
+        if(num2 == 0){
+            System.out.println("Não existe divisao por zero");
+            return;
+        }
+        System.out.println(num1/num2);
+
+
+
+    }
+
+    public void alteraDoisNumeros(int num1, int num2){
+        // vamos alterar o valor que estamos recebendo nos parametros
+        num1 = 99;
+        num2 = 33;
+        System.out.println("dentro do altera dois numeros");
+        System.out.println("Num1 "+num1 );
+        System.out.println("Num2 "+num2);
+    }
+
+
 }
--- src/academy/devdojo/maratonajavacore/Bintroducaometodos/test/CalculadoraTest03.java
+++ src/academy/devdojo/maratonajavacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -5,9 +5,14 @@ import academy.devdojo.maratonajavacore.Bintroducaometodos.dominio.Calculadora;
 public class CalculadoraTest03 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        double resultado = calculadora.divideDoisNumeros(20, 2); // pode ser feito assim
-        calculadora.divideDoisNumeros(20, 2);
+        double resultado =  calculadora.divideDoisNumeros(20, 0);
         System.out.println(resultado);
-        System.out.println(calculadora.divideDoisNumeros(20, 2)); // e também pode nao usar variaveis, para retornar o valor
+        System.out.println(calculadora.divideDoisNumeros02(20, 0));
+        System.out.println("-----------------");
+
+       calculadora.imprimeDivisaoDeDoisNumeros(86, 0);
+
+
+
     }
 }
